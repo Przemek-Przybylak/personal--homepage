5816b3d3d72a73c4d8acf9f84129d5d632603724
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2021-12-22 20:53:59 +0100

    create theme provider file

---
+++ src/theme.js
@@ -0,0 +1,12 @@
+export const theme = {
+  color: {
+    mercury: "#E5E5E5",
+    mineShaft: "#252525",
+    slateGray: "#6E7E91",
+    scienceBlue: "#0366D6",
+    white: "#FFFFFF",
+  },
+  breakpoint: {
+    mobileMax: "767px",
+  },
+};
