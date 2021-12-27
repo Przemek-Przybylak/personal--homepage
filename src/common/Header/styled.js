1d47b6b4949ce2d3fcb3295cd2e010aaf47d94fd
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2021-12-27 18:18:15 +0100

    added styles to Header component

--- src/common/Header/styled.js
+++ src/common/Header/styled.js
@@ -1,4 +1,17 @@
+import styled from "styled-components";
+
 export const Wrapper = styled.header`
-  display: grid;
-  grid-template-columns: 1fr, auto;
+  margin-top: 119px;
+  display: flex;
+`;
+
+export const Picture = styled.img`
+  width: 384px;
+  height: 384px;
+  background-color: black;
+`;
+
+export const AboutMe = styled.article`
+  align-self: center;
+  width: 633px;
 `;
