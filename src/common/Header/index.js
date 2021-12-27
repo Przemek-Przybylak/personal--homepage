1f80c81039991b34202cc32c9635481e5cba6a35
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2021-12-27 18:17:48 +0100

    added content to Header component

--- src/common/Header/index.js
+++ src/common/Header/index.js
@@ -1,3 +1,17 @@
+import { AboutMe, Picture, Wrapper } from "./styled";
+import { ThemeToggle } from "./ThemeToggle";
+
 export const Header = () => {
-  return <div>nanana</div>;
+  return (
+    <Wrapper>
+      <Picture />
+      <ThemeToggle />
+      <AboutMe>
+        lorem ipsum`
+        lalalalalalalalalalalalalalalallalalalalalalalalalalalalalal
+        lalalalalallalala lalalalalallala allalalalallala lalallalalalalal
+        lalalalal alalalla aallalaa a aja ja ja ja ja jaj aj ja ja ja ja ja j a
+      </AboutMe>
+    </Wrapper>
+  );
 };
