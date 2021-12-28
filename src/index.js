813c5379cdda1ff6ad82a9779f960e4d36f8e0db
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2021-12-28 16:16:34 +0100

    added store to app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { ThemeProvider } from "styled-components";
+import { Provider } from "react-redux";
 import { GlobalStyles } from "./GlobalStyles";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { theme } from "./theme";
+import { darkTheme, lightTheme } from "./theme";
+import store from "./store";
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      <App />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={darkTheme ? darkTheme : lightTheme}>
+        <GlobalStyles />
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
