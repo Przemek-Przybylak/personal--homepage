aa90505da41280ec5ca08171932495e3c98d933c
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2021-12-28 16:17:23 +0100

    create store.js

---
+++ src/store.js
@@ -0,0 +1,17 @@
+import { configureStore } from "@reduxjs/toolkit";
+import createSagaMiddleware from "redux-saga";
+import themeReducer from "./common/Header/ThemeToggle/ThemeSlice";
+import rootSaga from "./rootSaga";
+
+const sagaMiddleware = createSagaMiddleware();
+
+const store = configureStore({
+  reducer: {
+    theme: themeReducer,
+  },
+  middleware: [sagaMiddleware],
+});
+
+sagaMiddleware.run(rootSaga);
+
+export default store;
