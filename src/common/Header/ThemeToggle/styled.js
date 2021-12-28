bc712e3ac93052bf969fa3008df1d03f5be5cac1
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2021-12-28 13:56:00 +0100

    Header elements places added

--- src/common/Header/ThemeToggle/styled.js
+++ src/common/Header/ThemeToggle/styled.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Wrapper = styled.div`
   display: flex;
-  order: 3;
   height: 15px;
-  align-content: flex-end;
+  justify-content: flex-end;
+  grid-area: themeToggler / themeToggler / themeToggler / ThemeToggler;
 `;
--- src/common/Header/styled.js
+++ src/common/Header/styled.js
@@ -2,16 +2,19 @@ import styled from "styled-components";
 
 export const Wrapper = styled.header`
   margin-top: 119px;
-  display: flex;
+  display: grid;
+  gap: 66px;
+  grid-template-rows: 32px 1fr;
+  grid-template-areas: "photo themeToggler" "photo personalInfo";
 `;
 
 export const Picture = styled.img`
+  grid-area: photo / photo / photo / photo;
   width: 384px;
   height: 384px;
   background-color: black;
 `;
 
 export const AboutMe = styled.article`
-  align-self: center;
-  width: 633px;
+  grid-area: personalInfo / personalInfo / personalInfo / personalInfo;
 `;
