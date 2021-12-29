2c78959bfaa9f15b313d9cc850ba54e3332f4218
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2021-12-29 20:01:43 +0100

    added Theme and GlobalStyles in App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+import { useSelector } from "react-redux";
+import { ThemeProvider } from "styled-components";
 import { Container } from "./common/Container";
 import { Header } from "./common/Header";
+import { selectDarkTheme } from "./common/Header/ThemeToggle/ThemeSlice";
+import { GlobalStyles } from "./GlobalStyles";
+import { darkTheme, lightTheme } from "./theme";
 
 function App() {
+  const darkThemeOn = useSelector(selectDarkTheme);
+
   return (
     <div className="App">
-      <Container>
-        <Header />
-      </Container>
+      <ThemeProvider theme={darkThemeOn ? darkTheme : lightTheme}>
+        <GlobalStyles />
+        <Container>
+          <Header />
+        </Container>
+      </ThemeProvider>
     </div>
   );
 }
