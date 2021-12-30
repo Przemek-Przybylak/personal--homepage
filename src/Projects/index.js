d04b4a013eb7c3da93705404755da415890cb0db
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2021-12-30 22:56:49 +0100

    create Projects component

---
+++ src/Projects/index.js
@@ -0,0 +1,11 @@
+import { Content, Header, Logo, Wrapper } from "./styled";
+
+export const Projects = () => {
+  return (
+    <Wrapper>
+      <Logo />
+      <Header>portfolio</Header>
+      <Content>My recent projects</Content>
+    </Wrapper>
+  );
+};
