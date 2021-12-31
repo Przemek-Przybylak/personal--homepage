e944d7d7f333954fd28f36961ca5c7d4aeb1b28a
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2021-12-31 17:40:13 +0100

    Repositories Slice added

---
+++ src/features/RepoTiles/state/RepositoriesSlice.js
@@ -0,0 +1,37 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const repositoriesSlice = createSlice({
+  name: "repositories",
+  initialState: {
+    repositories: {},
+    loading: true,
+    error: false,
+  },
+  reducers: {
+    fetchRepositories: (state) => {
+      state.loading = true;
+      state.error = false;
+    },
+    fetchRepositoriesSuccess: (state, { payload: repositories }) => {
+      state.repositories = repositories;
+      state.loading = false;
+      state.error = false;
+    },
+    fetchRepositoriesError: (state) => {
+      state.loading = false;
+      state.error = true;
+    },
+  },
+});
+
+export const {
+  fetchRepositories,
+  fetchRepositoriesSuccess,
+  fetchRepositoriesError,
+} = repositoriesSlice.actions;
+
+export const selectRepositories = (state) => state.repositories.repositories;
+export const selectError = (state) => state.repositories.error;
+export const selectLoading = (state) => state.repositories.loading;
+
+export default repositoriesSlice.reducer;
