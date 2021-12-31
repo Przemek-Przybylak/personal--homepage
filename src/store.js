0dfe1cc26bd22f0f7a5fa4f482608966b832bf9c
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2021-12-31 17:38:10 +0100

    added repositoriesReducer in Store.js

--- src/store.js
+++ src/store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import createSagaMiddleware from "redux-saga";
 import themeReducer from "./common/Header/ThemeToggle/state/ThemeSlice";
 import rootSaga from "./rootSaga";
+import repositoriesReducer from "./features/RepoTiles/state/RepositoriesSlice";
 
 const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
   reducer: {
     theme: themeReducer,
+    repositories: repositoriesReducer,
   },
   middleware: [sagaMiddleware],
 });
