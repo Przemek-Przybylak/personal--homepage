9e2a6c133b62178ae8c2166ec1fb6002798cc8b4
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-01-02 22:41:40 +0100

    added setApplicationStatus in RepositoriesSaga

--- src/features/RepoTiles/state/RepositoriesSaga.js
+++ src/features/RepoTiles/state/RepositoriesSaga.js
@@ -1,19 +1,20 @@
 import { call, delay, put, takeLatest } from "redux-saga/effects";
 import { fetchRepositoriesFromApi } from "../fetchRepositoriesFromApi";
 import {
-  fetchRepositoriesError,
   fetchRepositoriesSuccess,
   fetchRepositories,
+  setApplicationStatus,
 } from "./RepositoriesSlice";
 
 function* fetchRepositoriesHandler() {
   try {
+    delay(1000);
     const repositories = yield call(fetchRepositoriesFromApi);
     yield delay(500);
-    console.log(repositories);
     yield put(fetchRepositoriesSuccess(repositories));
+    yield put(setApplicationStatus("success"));
   } catch (error) {
-    yield put(fetchRepositoriesError());
+    yield put(setApplicationStatus("error"));
   }
 }
 
