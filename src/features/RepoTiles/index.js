d2311f9f661f4cdaf2ae4353378441578c25f803
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-01-02 22:40:28 +0100

    added Loader ErrorPage in RepoTiles

--- src/features/RepoTiles/index.js
+++ src/features/RepoTiles/index.js
@@ -1,9 +1,12 @@
 import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
+import { ErrorPage } from "../../common/ErrorPage";
+import { Loader } from "../../common/Loader";
 import {
   fetchRepositories,
   selectRepositories,
+  selectStatus,
 } from "./state/RepositoriesSlice";
 import {
   Description,
@@ -18,36 +21,43 @@ import {
 export const RepoTiles = () => {
   const dispatch = useDispatch();
   const repositories = useSelector(selectRepositories);
+  const status = useSelector(selectStatus);
 
   useEffect(() => {
     dispatch(fetchRepositories());
-  }, []);
+  });
   return (
-    <Wrapper>
-      {repositories.map(({ id, name, description, html_url }) => (
-        <ProjectTile key={id}>
-          <Header>{name}</Header>
-          <Description>{description}</Description>
-          <LinksWrapper>
-            <LinkWrapper>
-              DEMO:
-              <Link
-                target="_blank"
-                rel="noreferrer"
-                href={`https://przemek-przybylak.github.io/${name}/`}
-              >
-                {`https://przemek-przybylak.github.io/${name}/`}
-              </Link>
-            </LinkWrapper>
-            <LinkWrapper>
-              Code:
-              <Link target="_blank" rel="noreferrer" href={html_url}>
-                {html_url}
-              </Link>
-            </LinkWrapper>
-          </LinksWrapper>
-        </ProjectTile>
-      ))}
-    </Wrapper>
+    <>
+      {status === "success" && (
+        <Wrapper>
+          {repositories.map(({ id, name, description, html_url }) => (
+            <ProjectTile key={id}>
+              <Header>{name}</Header>
+              <Description>{description}</Description>
+              <LinksWrapper>
+                <LinkWrapper>
+                  DEMO:
+                  <Link
+                    target="_blank"
+                    rel="noreferrer"
+                    href={`https://przemek-przybylak.github.io/${name}/`}
+                  >
+                    {`https://przemek-przybylak.github.io/${name}/`}
+                  </Link>
+                </LinkWrapper>
+                <LinkWrapper>
+                  Code:
+                  <Link target="_blank" rel="noreferrer" href={html_url}>
+                    {html_url}
+                  </Link>
+                </LinkWrapper>
+              </LinksWrapper>
+            </ProjectTile>
+          ))}
+        </Wrapper>
+      )}
+      {status === "loading" && <Loader />}
+      {status === "error" && <ErrorPage />}
+    </>
   );
 };
