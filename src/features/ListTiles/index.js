3081b691c1046fab1f76c87d8a52c32b0d5c7a2b
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-01-02 22:39:04 +0100

    added key to ListTile Element

--- src/features/ListTiles/index.js
+++ src/features/ListTiles/index.js
@@ -8,13 +8,12 @@ import {
 } from "./styled";
 
 export const ListTiles = ({ skills, title }) => {
-  console.log(skills);
   return (
     <Wrapper>
       <Caption>{title}</Caption>
       <List>
         {skills.map((skill) => (
-          <Element key={skill.id}>
+          <Element key={skill}>
             <Bullet />
             <ListItemText>{skill}</ListItemText>
           </Element>
